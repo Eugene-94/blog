d177d3095cdd2e55ffd6ad04a20c75b1db402143
Eugene Kaygorodov <gekak94@gmail.com>
2021-06-09 12:07:52 +0500

    post page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Post } from '../shared/interfaces';
+import { PostService } from '../shared/post.service';
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  posts$: Observable<Post>;
+
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.posts$ = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postService.getById(params.id);
+      })
+    );
   }
 
 }
